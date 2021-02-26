4a283017266d522266b2aa4ad8547a5a589e98a4
jdegregorio <joseph.degregorio@gmail.com>
2021-02-25 21:28:24 -0800

    Completed baseline dummy model, and setup a DVC pieline

--- code/03_model.R
+++ code/03_model.R
@@ -6,12 +6,15 @@
 
 # Import packages
 library(tidyverse)
+library(lubridate)
 library(here)
 library(arrow)
 library(rsample)
+library(yaml)
 
 # Source dummy model functions
 source(here("code", "funs_eval.R"))
+
 
 # LOAD DATA ---------------------------------------------------------------
 
@@ -81,18 +84,43 @@ df_eval <- grid_samples %>%
   select(id, data_eval) %>%
   unnest(data_eval)
 
-# Summarize metrics
+# Summarize metrics by forecast lead across samples
 df_eval_sum <- df_eval %>%
   group_by(pred_lead) %>%
   summarize(
     mape_raw = mean(abs(error_pct)),
     rmse_raw = sqrt(sum(error_raw^2) / n()),
-    rmse_weighted = sqrt(sum(error_weighted^2) / n())
+    rmse_wei = sqrt(sum(error_wei^2) / n())
   ) %>%
   ungroup()
+
+# Compute overall metrics
+mape_raw <- df_eval_sum %>% pull(mape_raw) %>% mean()
+rmse_raw <- df_eval_sum %>% pull(rmse_raw) %>% mean()
+rmse_wei <- df_eval_sum %>% pull(rmse_wei) %>% mean()
+
+# Summarize metrics in lists for tracking
+ls_mape_raw <- as.list(df_eval_sum$mape_raw)
+ls_rmse_raw <- as.list(df_eval_sum$rmse_raw)
+ls_rmse_wei <- as.list(df_eval_sum$rmse_wei)
+
+names(ls_mape_raw) <- df_eval_sum$pred_lead
+names(ls_rmse_raw) <- df_eval_sum$pred_lead
+names(ls_rmse_wei) <- df_eval_sum$pred_lead
+
+ls_pred_lead <- list(
+  mape_raw = ls_mape_raw,
+  rmse_raw = ls_rmse_raw,
+  rmse_wei = ls_rmse_wei
+)
+
+ls_metrics <- list(
+  pred_lead = ls_pred_lead,
+  overall = list(mape_raw = mape_raw, rmse_raw = rmse_raw, rmse_wei = rmse_wei)
+)
   
-# Plot MAPE
-p_mape <- 
+# Plot MAPE by forecast lead
+p_mape_lead <- 
   df_eval %>%
   ggplot(aes(y = fct_rev(as_factor(pred_lead)), x = error_pct)) +
   ggridges::geom_density_ridges(alpha = 0.25, fill = "black") +
@@ -104,7 +132,31 @@ p_mape <-
     y = "Prediction Lead (Days)"
   )
 
+# Plot MAPE by forecast year
+p_mape_year <- df_eval %>% 
+  mutate(year = year(date)) %>% 
+  ggplot(aes(y = fct_rev(as_factor(year)), x = error_pct)) +
+  ggridges::geom_density_ridges(alpha = 0.25, fill = "black") +
+  scale_x_continuous(labels = scales::percent, limits = c(-0.75, 0.75)) +
+  theme_light() +
+  labs(
+    title = "Percent Error Distribution by Over Time",
+    x = "Error Percentage",
+    y = "Prediction Year"
+  )
+
 # Save metrics
-ggsave(here("data", "metrics", "plot_mape.jpg"), plot = p_mape, height = 4, width = 6, dpi = 1000)
+write_yaml(ls_metrics, here("data", "metrics", "metrics.yaml"))
+ggsave(here("data", "metrics", "plot_mape_lead.jpg"), plot = p_mape_lead, height = 4, width = 6, dpi = 1000)
+ggsave(here("data", "metrics", "plot_mape_year.jpg"), plot = p_mape_year, height = 4, width = 6, dpi = 1000)
 write_parquet(df_eval, here("data", "metrics", "df_eval.parquet"))
 write_parquet(df_eval_sum, here("data", "metrics", "df_eval_sum.parquet"))
+
+
+# FIT MODEL (ALL DATA) ----------------------------------------------------
+
+# Train model
+model_fit <- train_dummy(df_all)
+
+# Save model
+write_rds(model_fit, here("data", "model", "model.rds"))
--- code/funs_eval.R
+++ code/funs_eval.R
@@ -5,10 +5,10 @@ evaluate_samples <- function(data_pred, data_test) {
     arrange(date) %>%
     mutate(
       pred_lead = 1:n(),
-      error_weight = 1 - ((pred_lead - 1) / max(pred_lead)),
+      weight = 1 - ((pred_lead - 1) / max(pred_lead)),
       error_raw = volume_total_actual - volume_total_pred,
       error_pct = error_raw / volume_total_actual,
-      error_weighted = error_raw * error_weight
+      error_wei = error_raw * weight
     )
   
   
